1ec3a0a11c51794551d63976d76e194c771a57f8
Malin Andersson <annamalinelisabet@gmail.com>
2023-08-31 20:32:04 +0200

    Add back button to detail view

--- src/views/QuizAppProject/QuizAppView.tsx
+++ src/views/QuizAppProject/QuizAppView.tsx
@@ -2,8 +2,13 @@ import { useEffect } from 'react'
 import './QuizAppView.css'
 import video from '../../assets/quiz-app.mp4'
 import screenshot from '../../assets/app-screenshot.jpg'
+import { BsArrowLeft } from 'react-icons/bs'
+import { MdOpenInNew } from 'react-icons/md'
+import { useNavigate } from 'react-router-dom'
 
 const QuizAppView = () => {
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     window.scrollTo(0, 0)
@@ -15,8 +20,12 @@ const QuizAppView = () => {
       <h1 className='header'>QUIZ APP</h1>
       <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium tempora, provident deleniti magni eveniet ad natus sapiente, quas vel, totam amet exercitationem mollitia deserunt saepe.</p>
       <p className='text2'>CODE&nbsp; - &nbsp;DESIGN</p>
-      <a href='https://github.com/annamalinelisabet/quiz-app' target='_blank' rel='noopener noreferrer' aria-label='Link to GitHub project'><button className='btn btn-code'>VIEW CODE</button></a>
-      <video src={video} width='220' height='480' poster={screenshot} autoPlay controls>Your browser can't play the video</video>
+      <a href='https://github.com/annamalinelisabet/quiz-app' target='_blank' rel='noopener noreferrer' aria-label='Link to GitHub project'><button className='btn btn-code'>VIEW CODE <MdOpenInNew /></button></a>
+      <video src={video} width='220' height='480' poster={screenshot} controls>Your browser can't play the video</video>
+      <div className='arrow-wrapper' onClick={() => navigate(-1)}>
+        <BsArrowLeft />
+        <p>Back</p>
+      </div>
     </div>
   )
 }
