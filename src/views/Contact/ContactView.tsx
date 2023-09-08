b32100dc7782c1b0142f6c6475af2fbe6661bc78
Malin Andersson <annamalinelisabet@gmail.com>
2023-09-08 21:41:49 +0200

    Test form again

--- src/views/Contact/ContactView.tsx
+++ src/views/Contact/ContactView.tsx
@@ -7,50 +7,61 @@ import illustration from '../../assets/illustration-wave.jpg'
 import { HiArrowLongDown } from 'react-icons/hi2'
 import { AiFillLinkedin } from 'react-icons/ai'
 
-const ContactView = () => {
-  
-  useEffect(() => {
-    window.scrollTo(0, 0)
-  }, [])
-
+const Form = () => {
   const { executeRecaptcha } = useGoogleReCaptcha()
 
   const [state, handleSubmit] = useForm('xdorzwze', {
     data: { 'g-recaptcha-response': executeRecaptcha }
   })
 
+  return state.succeeded ? (
+    <p>Message sent!</p>
+  ) : (
+    <form className='flex-column' onSubmit={handleSubmit}>
+    <div className='input-control flex-column'>
+      <label htmlFor='name'>Your name</label>
+      <input id='name' type='text' name='name' required />
+    </div>
+    <div className='input-control flex-column'>
+      <label htmlFor='email'>Your email</label>
+      <input id='email' type='email' name='email' required />
+    </div>
+    <div className='input-control flex-column'>
+      <label htmlFor='message'>Message</label>
+      <textarea id='message' name='message' className='message' required></textarea>
+    </div>
+    <button className='btn btn-send' disabled={state.submitting}>SEND</button>
+    { state.succeeded ? <p className='text'>Message sent!</p> : null }
+    <ValidationError className='error' errors={state.errors}/>
+  </form>
+  )
+}
+
+
+const ContactView = () => {
+  
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [])
+
+
+
   return (
     <div className='ContactView container pb-2 animate__animated animate__fadeIn animate__slower'>
-      <h1>SAY HI</h1>
-      <div className='wrapper'>
-        <div className='illustration-box'>
-          <img src={illustration} alt='Illustration of a person waving' />
+        <h1>SAY HI</h1>
+        <div className='wrapper'>
+          <div className='illustration-box'>
+            <img src={illustration} alt='Illustration of a person waving' />
+          </div>
+          <h4>and let's work together</h4>
         </div>
-        <h4>and let's work together</h4>
-      </div>
-      
-      <p className='text-xs'>Don't hesitate to get in touch with me – I've made it incredibly simple! Just drop a message via the contact form provided below. But if that's not your style, feel free to shoot me an email at <a href='mailto:annamalinelisabet@gmail.com' className='email'>annamalinelisabet@gmail.com</a> or connect with me on <span className='linkedin'>LinkedIn.<a href='https://www.linkedin.com/in/anna-malin-elisabet-andersson' target='_blank' rel='noopener noreferrer'><AiFillLinkedin className='li-icon'/></a><HiArrowLongDown className='arrow'/></span></p>
+        
+        <p className='text-xs'>Don't hesitate to get in touch with me – I've made it incredibly simple! Just drop a message via the contact form provided below. But if that's not your style, feel free to shoot me an email at <a href='mailto:annamalinelisabet@gmail.com' className='email'>annamalinelisabet@gmail.com</a> or connect with me on <span className='linkedin'>LinkedIn.<a href='https://www.linkedin.com/in/anna-malin-elisabet-andersson' target='_blank' rel='noopener noreferrer'><AiFillLinkedin className='li-icon'/></a><HiArrowLongDown className='arrow'/></span></p>
+        <GoogleReCaptchaProvider reCaptchaKey='6LfTrg0oAAAAAA_CYhaFsW-Jr9UxxeUYTIeSGzU-'>
+          <Form />
 
-      <GoogleReCaptchaProvider reCaptchaKey='6LfTrg0oAAAAAA_CYhaFsW-Jr9UxxeUYTIeSGzU-'>
-        <form className='flex-column' onSubmit={handleSubmit}>
-          <div className='input-control flex-column'>
-            <label htmlFor='name'>Your name</label>
-            <input id='name' type='text' name='name' required />
-          </div>
-          <div className='input-control flex-column'>
-            <label htmlFor='email'>Your email</label>
-            <input id='email' type='email' name='email' required />
-          </div>
-          <div className='input-control flex-column'>
-            <label htmlFor='message'>Message</label>
-            <textarea id='message' name='message' className='message' required></textarea>
-          </div>
-          <button className='btn btn-send' disabled={state.submitting}>SEND</button>
-          { state.succeeded ? <p className='text'>Message sent!</p> : null }
-          <ValidationError className='error' errors={state.errors}/>
-        </form>
-      </GoogleReCaptchaProvider>
-    </div>
+        </GoogleReCaptchaProvider>
+      </div>
   )
 }
 
